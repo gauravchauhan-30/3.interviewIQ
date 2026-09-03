fb1283c6e082579fe8cc0eb1b328d405a1192017
gauravchauhan-30 <gaurav.chauhanbina@gmail.com>
2026-09-04 00:18:49 +0530

    Update index.js

--- 3.interviewIQ/server/index.js
+++ 3.interviewIQ/server/index.js
@@ -15,8 +15,11 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://three-interviewiq-1-2g0t.onrender.com",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://three-interviewiq-1-2g0t.onrender.com"
+    ],
+    credentials: true
 }))
 
 app.use(express.json())
